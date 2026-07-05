6d4ee71aabbf3c8af2dd0805129f433eb8c78416
Tiens0710 <tengiadd7@gmail.com>
2026-07-05 23:20:02 +0700

    chore: change TRELLIS attention backend to sdpa for better compatibility

--- src/generator_3d.py
+++ src/generator_3d.py
@@ -21,8 +21,8 @@ import os, json, torch
 sys.modules['triton'] = None
 
 os.environ["SPCONV_ALGO"] = "native"
-os.environ["ATTN_BACKEND"] = "xformers"
-os.environ["SPARSE_ATTN"] = "xformers"
+os.environ["ATTN_BACKEND"] = "sdpa"
+os.environ["SPARSE_ATTN"] = "sdpa"
 os.environ["MPLBACKEND"] = "agg"
 
 if not os.path.exists("/kaggle/working/TRELLIS/trellis"):
